2233a2dcb33ac062959c6e16f0caff1e4fab2d8c
Justin Hurstwright <jryanhurst@gmail.com>
2021-11-27 21:48:49 -0600

    feat: Player can mode

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,27 @@ struct Player {
 }
 
 impl Player {
-    fn new(x: i32, y: i32) -> Self {
+    pub fn new(x: i32, y: i32) -> Self {
         Self {
             position: Point::new(x, y),
             color: ColorPair::new(GREEN, BLACK),
         }
     }
 
-    fn render(&self, draw: &mut DrawBatch) {
+    pub fn update(&mut self, ctx: &mut BTerm) {
+        if let Some(key) = ctx.key {
+            let delta = match key {
+                VirtualKeyCode::Left => Point::new(-1, 0),
+                VirtualKeyCode::Right => Point::new(1, 0),
+                VirtualKeyCode::Up => Point::new(0, -1),
+                VirtualKeyCode::Down => Point::new(0, 1),
+                _ => Point::new(0, 0)
+            };
+            self.position += delta;
+        }
+    }
+
+    pub fn render(&self, draw: &mut DrawBatch) {
         draw.set(self.position, self.color, to_cp437('@'));
     }
 }
@@ -37,10 +50,8 @@ impl State {
             player: Player::new(DIMENSION_WIDTH / 2, DIMENSION_HEIGHT / 2),
         }
     }
-}
 
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut bracket_lib::prelude::BTerm) {
+    fn render_tick(&mut self, ctx: &mut BTerm) {
         let mut draw = DrawBatch::new();
         draw.target(0);
         draw.cls();
@@ -51,6 +62,14 @@ impl GameState for State {
         render_draw_buffer(ctx).expect("Render Error");
     }
 }
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut bracket_lib::prelude::BTerm) {
+        self.player.update(ctx);
+        self.render_tick(ctx);
+    }
+}
+
 
 fn main() -> BError {
     let mut context = BTermBuilder::simple80x50()
