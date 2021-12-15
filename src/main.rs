122dc380a221064e228b3638cd87875b7c688627
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-12-14 21:40:25 -0700

    refactor: Extract module

--- src/main.rs
+++ src/main.rs
@@ -28,54 +28,6 @@ mod prelude {
 }
 use crate::prelude::*;
 
-#[system]
-pub fn world_gen(
-    #[resource] rng: &mut WorldGenRng,
-    #[resource] generator: &mut GeneraotrRunner,
-    #[resource] builder: &mut MapBuilder,
-    commands: &mut CommandBuffer,
-) {
-    if !generator.is_finished() {
-        generator.next(builder, rng);
-        return;
-    }
-    let MapResult { map, player } = builder.build_map();
-    let player = player.expect("Failed to place player in worlds");
-    spawn_player(commands, player);
-    builder
-        .rooms
-        .iter()
-        .filter(|room| room.center() != player)
-        .map(|r| r.center())
-        .for_each(|pos| {
-            spawn_monster(commands, rng, pos);
-        });
-    commands.exec_mut(move |_, resources| {
-        resources.insert(map.clone());
-        resources.insert(Camera::new(player));
-    });
-}
-
-#[system]
-pub fn builder_render(#[resource] builder: &mut MapBuilder) {
-    let mut draw_batch = DrawBatch::new();
-    draw_batch.target(0);
-    for tile in builder.fill_tile.iter() {
-        let region = Rect::with_size(0, 0, builder.width, builder.height);
-        draw_batch.fill_region(region, ColorPair::new(YELLOW, BLACK), *tile);
-    }
-    for room in builder.rooms.iter() {
-        draw_batch.fill_region(*room, ColorPair::new(RED, BLACK), TileType::Floor);
-    }
-    for tunnel in builder.tunnels.iter() {
-        tunnel.render(&mut draw_batch);
-    }
-    for player in builder.player.iter() {
-        draw_batch.set(*player, ColorPair::new(GREEN, BLACK), to_cp437('@'));
-    }
-    draw_batch.submit(0).expect("Batch Error");
-}
-
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(systems::player_input_system())
@@ -87,8 +39,8 @@ pub fn build_scheduler() -> Schedule {
 
 pub fn build_build_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(world_gen_system())
-        .add_system(builder_render_system())
+        .add_system(systems::world_gen_system())
+        .add_system(systems::builder_render_system())
         .build()
 }
 
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,8 +2,10 @@ mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod world_gen;
 
 pub use collisions::collisions_system;
 pub use entity_render::entity_render_system;
 pub use map_render::map_render_system;
 pub use player_input::player_input_system;
+pub use world_gen::{builder_render_system, world_gen_system};
