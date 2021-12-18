eec8f5e425762051e244ad4ac1d131eb5a40f620
Justin Hurstwright <jryanhurst@gmail.com>
2021-12-18 13:11:05 -0600

    refactor: Move the renders stuff into own module

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -1,6 +1,23 @@
 use crate::prelude::*;
 
 #[system]
+#[read_component(Point)]
+#[read_component(Render)]
+pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(1);
+    let offset = camera.top_left_corner();
+
+    <(&Point, &Render)>::query()
+        .iter(ecs)
+        .for_each(|(pos, render)| {
+            draw_batch.set(*pos - offset, render.color, render.glyph);
+        });
+
+    draw_batch.submit(5000).expect("Batch Error");
+}
+
+#[system]
 pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
