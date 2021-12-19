ce21c3d91c80ec8f22fde8550f185cb5b72af7d0
Justin Hurstwright <jryanhurst@gmail.com>
2021-12-18 18:05:10 -0600

    refactor: Remove dead code

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -58,24 +58,15 @@ impl GeneratorCommand {
 
 pub struct GeneraotrRunner {
     pub commands: Vec<GeneratorCommand>,
-    pub system_progress: Progress,
-    pub sub_system_progress: Option<Progress>,
     pub run_index: usize,
 }
 
 impl GeneraotrRunner {
     pub fn new(commands: Vec<GeneratorCommand>) -> Self {
-        let total = commands.iter().fold(0, |total, cmd| cmd.steps() + total);
         Self {
             commands,
             run_index: 0,
-            system_progress: Progress { total, current: 0 },
-            sub_system_progress: None,
         }
-    }
-
-    pub fn is_finished(&self) -> bool {
-        self.system_progress.is_finished()
     }
 
     pub fn current_step(&self) -> i32 {
@@ -88,38 +79,34 @@ impl GeneraotrRunner {
     }
 
     pub fn total_steps(&self) -> i32 {
-        self.system_progress.total as i32
+        self.commands
+            .iter()
+            .fold(0, |total, cmd| cmd.steps() as i32 + total)
     }
 
     fn get_subsystem_current_progress(&self, index: usize) -> i32 {
         if index != self.run_index {
             return self.commands[index].steps() as i32;
         }
-        match self.sub_system_progress {
-            Some(Progress { current, .. }) => current as i32,
-            None => 0,
-        }
-    }
-
-    pub fn update_progress(&mut self) {
-        self.system_progress.current = self.current_step() as usize;
+        0
     }
 
     pub fn next(&mut self, builder: &mut MapBuilder, rng: &mut RandomNumberGenerator) {
-        self.update_progress();
-        if self.run_index >= self.commands.len() {
+        if self.is_finished() {
             return;
         }
         let perform = self.commands[self.run_index].perform(builder, rng);
-        self.sub_system_progress = perform.into();
-        self.system_progress.current = self.current_step() as usize;
         if let BuildCommandResult::Finished = perform {
             self.run_index += 1;
         }
     }
 
+    pub fn is_finished(&self) -> bool {
+        self.run_index >= self.commands.len()
+    }
+
     pub fn get_current_command(&self) -> Option<GeneratorCommand> {
-        if self.run_index >= self.commands.len() {
+        if self.is_finished() {
             return None;
         }
         Some(self.commands[self.run_index])
