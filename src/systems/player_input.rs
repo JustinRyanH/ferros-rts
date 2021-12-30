5d5fe8acf27feac9cf4bdf56dc47853f09081c2c
Justin Ryan Hurst <jryanhurst@gmail.com>
2021-12-30 12:36:55 -0600

    feat: Don't stand over a unit that player is attacking

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -9,26 +9,50 @@ pub fn player_input(
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] turn_state: &mut TurnState,
 ) {
-    if let Some(key) = key {
-        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-        let delta = match key {
-            VirtualKeyCode::Left | VirtualKeyCode::H => Point::new(-1, 0),
-            VirtualKeyCode::Right | VirtualKeyCode::L => Point::new(1, 0),
-            VirtualKeyCode::Up | VirtualKeyCode::K => Point::new(0, -1),
-            VirtualKeyCode::Down | VirtualKeyCode::J => Point::new(0, 1),
-            _ => Point::new(0, 0),
-        };
+    let key = match key {
+        Some(it) => it,
+        _ => return,
+    };
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    let delta = match key {
+        VirtualKeyCode::Left | VirtualKeyCode::H => Point::new(-1, 0),
+        VirtualKeyCode::Right | VirtualKeyCode::L => Point::new(1, 0),
+        VirtualKeyCode::Up | VirtualKeyCode::K => Point::new(0, -1),
+        VirtualKeyCode::Down | VirtualKeyCode::J => Point::new(0, 1),
+        _ => Point::new(0, 0),
+    };
 
-        players.iter(ecs).for_each(|(entity, pos)| {
-            let destination = *pos + delta;
+    let (player_entity, destination) = players
+        .iter(ecs)
+        .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+        .unwrap();
+
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+    if delta.x != 0 || delta.y != 0 {
+        let mut hit_something = false;
+        enemies
+            .iter(ecs)
+            .filter(|(_, pos)| **pos == destination)
+            .for_each(|(entity, _)| {
+                hit_something = true;
+                commands.push((
+                    (),
+                    WantsToAttack {
+                        attacker: player_entity,
+                        victim: *entity,
+                    },
+                ));
+            });
+        if !hit_something {
             commands.push((
                 (),
                 WantsToMove {
-                    entity: *entity,
+                    entity: player_entity,
                     destination,
                 },
             ));
-        });
-        *turn_state = TurnState::PlayerTurn;
+        }
     }
+
+    *turn_state = TurnState::PlayerTurn;
 }
